fbb39e238ac9552ca477e5994a59f308275f006d
SungHyun (Johnny) Tae <sh.tae@alcherainc.com>
2021-09-23 09:45:18 +0900

    testing gh actions#2

--- babel.config.js
+++ babel.config.js
@@ -15,7 +15,6 @@ const buildPresets = [
 const buildPlugins = [
   // ["@babel/plugin-proposal-class-properties", { loose: false }],
   // ["@babel/plugin-proposal-private-methods", { loose: false }],
-  // just testing github action
   ["@babel/plugin-proposal-private-property-in-object", { loose: true }]
 ];
 
